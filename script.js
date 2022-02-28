1be1a82b29e956f6b6e82c424ad81024c554c0f3
TALBI oussama <73158950+TALBI-Oussama@users.noreply.github.com>
2022-02-28 09:14:37 +0100

    changement de thème
    bouton qui change le thème de la page

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
+let section = document.querySelector("body");
 let sun = document.querySelector(".bx-sun");
+/* let moon = document.querySelector(".bx-sun"); */
 
-sun.onclick = function() {
-    sun.classList.remove("bx-sun");
-    sun.classList.add("bx-moon");
-}
+sun.addEventListener("click" , () =>{
+    if(section.classList.contains('dark')){
+        section.classList.add("light");
+        section.classList.remove("dark");
+        sun.classList.remove("bx-sun");
+        sun.classList.add("bx-moon");
+    } else if(section.classList.contains('light')){
+        section.classList.add("dark");
+        section.classList.remove("light");
+        sun.classList.add("bx-sun");
+        sun.classList.remove("bx-moon");
+    }
+});
